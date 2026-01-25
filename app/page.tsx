78b86eab7848c1e357f94d9124e3b56a3059e8a2
Dean <deaneffi1414@gmail.com>
2026-01-25 18:37:14 +0200

    basic setup and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>zehava's portfolio</h1>
-      <p>
+    <div className="font-semibold md:text-2xl lg:text-3xl text-purple-dark space-y-6 p-10 bg-purple-light min-h-screen">
+      <h1>{"Zahava's "}portfolio</h1>
+      <p className="max-w-[50%] mt-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Perspiciatis, earum. Odit perspiciatis repellat blanditiis
         harum eum, magni libero saepe ducimus sint reprehenderit sed
@@ -10,5 +10,5 @@ export default function Home() {
         ipsum!
       </p>
     </div>
-  );
+  )
 }
