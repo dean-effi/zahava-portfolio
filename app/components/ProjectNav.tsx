0e4534c4f36dec85fafc9d919b19d2bd3ee315c6
Dean <deaneffi1414@gmail.com>
2026-02-03 10:42:30 +0200

    fix project navbar alignment

--- app/components/ProjectNav.tsx
+++ app/components/ProjectNav.tsx
@@ -9,18 +9,24 @@ export default function ProjectNav({
 }) {
   return (
     <nav className="sticky top-0 z-20 w-full border-b-1 border-black bg-neutral-100 px-2 md:border-b-1 md:px-4">
-      <ul className="m-auto flex w-full max-w-[1100px] justify-between p-5 px-3 text-base md:px-5 md:text-lg lg:text-xl xl:max-w-[85%] xl:px-0">
-        <li className="text-left md:w-[10ch]">
+      <ul className="m-auto grid w-full max-w-[1100px] grid-cols-3 justify-items-center p-5 px-3 text-base md:px-5 md:text-lg lg:text-xl xl:max-w-[85%] xl:px-0">
+        <div className="justify-self-start">
           {previousPage && (
-            <Link href={previousPage}>⟵ previous</Link>
+            <li className="text-left">
+              <Link href={previousPage}>⟵ previous</Link>
+            </li>
           )}
-        </li>
+        </div>
         <li className="font-medium italic">
           <Link href={"/"}>Main Page</Link>
         </li>
-        <li className="text-right md:w-[10ch]">
-          {nextPage && <Link href={nextPage}>next ⟶</Link>}
-        </li>
+        <div className="justify-self-end">
+          {nextPage && (
+            <li>
+              <Link href={nextPage}>next ⟶</Link>
+            </li>
+          )}
+        </div>
       </ul>
     </nav>
   );
